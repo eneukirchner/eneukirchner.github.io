636e32d250998330b77db3285d4bd224c048f60f
Edgar Neukirchner <edgar.neukirchner@gmail.com>
2021-10-31 23:52:26 +0100

    Python net added

---
+++ python/python_net.py
@@ -0,0 +1,11 @@
+---
+sort: 2
+---
+
+# Netzwerkprogramme mit Python
+
+http://www.netzmafia.de/skripten/inetprog/serverclient.gif
+
+[Dokumentation Python Socket](https://docs.python.org/3/library/socket.html#module-socket)
+
+
